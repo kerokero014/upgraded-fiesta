c271116d9cbe10c87e8278c124da89571cec862b
kerokero014 <kevinjrm.kr@gmail.com>
2024-03-13 12:47:47 -0600

    Refactor Navbar component

--- NationalPark/src/lib/Navbar.svelte
+++ NationalPark/src/lib/Navbar.svelte
@@ -1,28 +1,27 @@
 <script>
-    let links = [
-      { name: "Home", url: "/" },
-      { name: "About", url: "/about" },
-      { name: "Contact", url: "/contact" },
-      { name: "Plan Trip", url: "/Trip" },
-      { name: "Sing Up", url: "/singup" },
-    ];
-  
-    let activeLink = "/";
-  </script>
-  
-  <nav>
-    {#each links as link (link.url)}
-      <a href={link.url} class:active={activeLink === link.url}>{link.name}</a>
-    {/each}
-  </nav>
-  
-  <style>
-    a {
-      margin-right: 10px;
-    }
-  
-    .active {
-      color: red;
-    }
-  </style>
-  
+  let links = [
+    { name: "Home", url: "/" },
+    { name: "About", url: "/about" },
+    { name: "Contact", url: "/contact" },
+    { name: "Plan Trip", url: "/Trip" },
+    { name: "Sing Up", url: "/singup" },
+  ];
+
+  let activeLink = "/";
+</script>
+
+<nav>
+  {#each links as link (link.url)}
+    <a href={link.url} class:active={activeLink === link.url}>{link.name}</a>
+  {/each}
+</nav>
+
+<style>
+  a {
+    margin-right: 10px;
+  }
+
+  .active {
+    color: red;
+  }
+</style>
