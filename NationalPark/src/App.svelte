914fc37d69ae90522181c50e510439c228ed4dff
kerokero014 <kevinjrm.kr@gmail.com>
2024-03-13 12:44:28 -0600

    Add Navbar and Header components, remove Counter component

--- NationalPark/src/App.svelte
+++ NationalPark/src/App.svelte
@@ -1,7 +1,11 @@
 <script>
+  import Navbar from './lib/Navbar.svelte';
+  import Header from './lib/MainHeader.svelte';
 </script>
 
+<Navbar />
+<Header title="Summit Seeker" />
+
 <main>
-  <h1>Summit Seeker</h1>
   <h2>National Park</h2>
 </main>
---
+++ NationalPark/src/lib/MainHeader.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let title = 'Default Title';
+</script>
+
+<header>
+    <h1>{title}</h1>
+</header>
