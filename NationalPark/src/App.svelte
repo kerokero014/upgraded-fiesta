1fedc64d349af9feb9b58c0a410ba3a84282bcaf
kerokero014 <kevinjrm.kr@gmail.com>
2024-03-13 19:15:27 -0600

    Update App.svelte with new header and footer components

--- NationalPark/src/App.svelte
+++ NationalPark/src/App.svelte
@@ -1,11 +1,14 @@
 <script>
   import Navbar from './lib/Navbar.svelte';
   import Header from './lib/MainHeader.svelte';
+  import Footer from './lib/MainFooter.svelte';
 </script>
 
 <Navbar />
-<Header title="Summit Seeker" />
- 
+<Header title="Summit Seekers" />
+
 <main>
-  <h2>National Park</h2>
+  <h2>Welcome to National Park</h2>
 </main>
+
+<Footer companyName="Summit Seekers" />
---
+++ NationalPark/src/lib/MainFooter.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let year = new Date().getFullYear();
+    export let companyName = 'Default Company';
+</script>
+
+<footer>
+    <p>© {year} {companyName}. All rights reserved.</p>
+</footer>
+
+<style>
+    footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #f8f9fa;
+        color: black;
+        text-align: center;
+        padding: 10px 0;
+    }
+</style>
