cbb2030adf86c4ecae68889988c9e93beffb5345
kerokero014 <kevinjrm.kr@gmail.com>
2024-03-18 19:20:25 -0600

    Update ContactForm.svelte styles and add button class

--- NationalPark/src/lib/ContactForm.svelte
+++ NationalPark/src/lib/ContactForm.svelte
@@ -74,7 +74,7 @@
           <span>Please enter a message</span>
         </div>
         <div class="inputBox w50">
-          <button on:click={sendMessage}>Send</button>
+          <button class="button" on:click={sendMessage}>Send</button>
         </div>
       </div>
     </div>
@@ -89,7 +89,7 @@
   :root {
     --black: #000;
     --white: #ffffffe7;
-    --lightergrey: #d6d6d6;
+    --lightergrey: #ecf5e8;
     --darkergrey: #333;
     --quiteblue: #293d31;
     --red: #ff4136;
@@ -322,6 +322,71 @@
 
     .container .contactusform .formBox {
       flex-direction: row;
+    }
+
+    .button {
+      appearance: button;
+      background-color: #003329;
+      border: solid transparent;
+      border-radius: 16px;
+      border-width: 0 0 4px;
+      box-sizing: border-box;
+      color: #ffffff;
+      cursor: pointer;
+      display: inline-block;
+      font-family: din-round, sans-serif;
+      font-size: 15px;
+      font-weight: 700;
+      letter-spacing: 0.8px;
+      line-height: 20px;
+      margin: 0;
+      outline: none;
+      overflow: visible;
+      padding: 13px 16px;
+      text-align: center;
+      text-transform: uppercase;
+      touch-action: manipulation;
+      transform: translateZ(0);
+      transition: filter 0.2s;
+      user-select: none;
+      -webkit-user-select: none;
+      vertical-align: middle;
+      white-space: nowrap;
+      width: 100%;
+    }
+
+    .button:after {
+      background-clip: padding-box;
+      background-color: #293d31;
+      border: solid transparent;
+      border-radius: 16px;
+      border-width: 0 0 4px;
+      bottom: -4px;
+      content: "";
+      left: 0;
+      position: absolute;
+      right: 0;
+      top: 0;
+      z-index: -1;
+    }
+
+    .button:main,
+    .button:focus {
+      user-select: auto;
+    }
+
+    .button:hover:not(:disabled) {
+      filter: brightness(1.1);
+      -webkit-filter: brightness(1.1);
+    }
+
+    .button:disabled {
+      cursor: auto;
+    }
+
+    .button:active {
+      border-width: 4px 0 0;
+      background: none;
     }
   }
 </style>
