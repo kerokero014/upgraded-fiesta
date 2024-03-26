5514e4ca12957739993b583504c7842bd28ea666
Ross Gardner <gar17014@byui.edu>
2024-03-26 17:25:58 -0600

    Created JSON file for Supplies list

--- NationalPark/src/lib/Supplies.svelte
+++ NationalPark/src/lib/Supplies.svelte
@@ -5,17 +5,19 @@
 
   let suppliesList = [];
 
-  const baseURL = "https://nationalparkplanner.onrender.com/supplies";
+  // const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
   // const apiKey = "4NkX3tOUbCHhiaSreQql8y0rvhstMo2Z4zWOWR3C";
 
   async function fetchData() {
       try {
-        const res = await fetch(`${baseURL}`);
+        const res = await fetch("/json/supplies.json");
         if (!res.ok) {
           throw new Error("Server response wasn't OK");
         }
         const data = await res.json();
-        suppliesList = data.data.map(item => item.name);
+        suppliesList = data.supplies_list;
+        console.log(suppliesList);
+        // suppliesList = data.data.map(item => item.name);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -28,11 +30,15 @@
 
 <Header title="Supplies" />
 
-<ul>
-  {#each suppliesList as item}
-    <li>{item}</li>
-  {/each}
-</ul>
+{#if suppliesList.length > 0}
+  <ul>
+    {#each suppliesList as item}
+      <li>{item.name}</li>
+    {/each}
+  </ul>
+{:else}
+  <p>Loading...</p>
+{/if}
 
 <Footer />
 
