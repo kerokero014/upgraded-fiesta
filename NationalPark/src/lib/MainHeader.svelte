ebaabc642e0eb9188c883d27ecd99f52691a61c0
kerokero014 <kevinjrm.kr@gmail.com>
2024-04-02 14:41:44 -0600

    Update MainHeader.svelte with logo image

--- NationalPark/src/lib/MainHeader.svelte
+++ NationalPark/src/lib/MainHeader.svelte
@@ -1,16 +1,18 @@
 <script>
-  export let title = "Default Title";
   import Navbar from "../lib/Navbar.svelte";
+  let imagePath = "../src/imgs/logo.jpg";
+  let imageAltText = "Summit Seekers Logo";
 </script>
 
 <header>
-  <h1 class="logo">{title}</h1>
+  <h1 class="logo"><img src={imagePath} alt={imageAltText} /></h1>
   <Navbar />
 </header>
 
 <style>
-  .logo {
+  .logo img {
     font-size: 2rem;
     margin: 10px 3px;
+    width: 20%;
   }
 </style>
