9986260e4d96372d61bcfc44c2edec175386cb91
kerokero014 <kevinjrm.kr@gmail.com>
2024-04-02 14:45:18 -0600

    Update background color and logo image path

--- NationalPark/src/lib/MainHeader.svelte
+++ NationalPark/src/lib/MainHeader.svelte
@@ -1,6 +1,6 @@
 <script>
   import Navbar from "../lib/Navbar.svelte";
-  let imagePath = "../src/imgs/logo.jpg";
+  let imagePath = "../src/imgs/logo.png";
   let imageAltText = "Summit Seekers Logo";
 </script>
 
@@ -11,8 +11,6 @@
 
 <style>
   .logo img {
-    font-size: 2rem;
-    margin: 10px 3px;
     width: 20%;
   }
 </style>
