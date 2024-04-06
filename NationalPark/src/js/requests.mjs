72b455a8156bfc4c147f888014ff41dc764a0c9c
kerokero014 <kevinjrm.kr@gmail.com>
2024-04-05 20:15:38 -0600

    Update styles in app.css and Supplies.svelte

--- NationalPark/src/js/requests.mjs
+++ NationalPark/src/js/requests.mjs
@@ -1,17 +1,38 @@
 export async function fetchDatafromAPI() {
-    try {
-        const response = await fetch("https://npp-api.onrender.com/user");
-        console.log(response);
-        
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+  try {
+    const response = await fetch("https://npp-api.onrender.com/user");
+    console.log(response);
 
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('There was a problem with the fetch operation: ', error);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation: ", error);
+  }
 }
 
+export async function PostDataSupplyAPI(data) {
+  try {
+    const response = await fetch("https://npp-api.onrender.com/supplies/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation: ", error);
+  }
+}
 
---
+++ NationalPark/src/lib/SupplyForm.svelte
@@ -0,0 +1,105 @@
+<script>
+  import { PostDataSupplyAPI } from "../js/requests.mjs";
+  let name = "";
+  let quantity = 0;
+  let price = "";
+  let description = "";
+  let category = [];
+  let website = "";
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const data = {
+      name,
+      quantity,
+      price,
+      description,
+      category,
+      website,
+    };
+    console.log(data);
+    const response = await PostDataSupplyAPI(data);
+    console.log(response);
+  }
+
+
+  function showMessage() {
+    alert("Supply added successfully!");
+  }
+</script>
+
+<div class="card">
+  <h1>Add a Supply!</h1>
+  <form on:submit={handleSubmit}>
+    <label for="name">Name:</label>
+    <input type="text" id="name" bind:value={name} required />
+
+    <label for="quantity">Quantity:</label>
+    <input type="number" id="quantity" bind:value={quantity} required />
+
+    <label for="price">Price:</label>
+    <input type="text" id="price" bind:value={price} required />
+
+    <label for="description">Description:</label>
+    <textarea id="description" bind:value={description} required></textarea>
+
+    <label for="category">Category:</label>
+    <input type="text" id="category" bind:value={category} required />
+
+    <label for="website">Website:</label>
+    <input type="text" id="website" bind:value={website} required />
+
+    <button type="submit" on:click={showMessage}>Add Supply</button>
+  </form>
+</div>
+
+<style>
+  .card {
+    background-color: #ffffff7a;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    margin: 0px;
+    width: 700px;
+  }
+  h1 {
+    text-align: center;
+    margin: 20px 0;
+    font-size: 38px;
+    color: #004d40; /* Teal color */
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 20px 0;
+  }
+
+  label {
+    margin-top: 10px;
+    font-size: 30px;
+  }
+
+  input,
+  textarea {
+    margin-bottom: 10px;
+    padding: 5px;
+    width: 300px;
+    font-size: larger;
+  }
+
+  button {
+    margin-top: 20px;
+    padding: 10px;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #0056b3;
+  }
+</style>
